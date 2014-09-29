f797a3ea3ee86e713a3bdd5e6034e75822a5debe
David Van Der Beek <earlynovrock@gmail.com>
2014-09-29 10:00:08 -0500

    update default to email

--- lib/contact.rb
+++ lib/contact.rb
@@ -3,7 +3,7 @@ require "contact/engine"
 module Contact
   class << self
     mattr_accessor :to_email
-    self.to_email = "david@flyoverworks.com"
+    self.to_email = "user@example.com"
   end
 
   def self.configure(&block)
