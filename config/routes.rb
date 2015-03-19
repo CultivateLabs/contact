6997c708b5ca29d707151e9aa3ff70b9d6a3c7a0
David Van Der Beek <earlynovrock@gmail.com>
2015-03-19 11:55:01 -0500

    add setting for contact page path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Contact::Engine.routes.draw do
-  get :contact, to: 'messages#new', as: :contact
+  get "/#{Contact.contact_page_path}", to: 'messages#new', as: :contact
   post 'messages/create', as: :messages
 end
--- lib/flyover-contact.rb
+++ lib/flyover-contact.rb
@@ -1,11 +1,12 @@
 require "contact/engine"
 
 module Contact
-  mattr_accessor :to_email, :subject, :success_message, :base_controller
+  mattr_accessor :to_email, :subject, :success_message, :base_controller, :contact_page_path
   @@to_email = "to@example.com"
   @@subject = "New Contact Form Submission"
   @@success_message = "Success! Your message has been sent and we'll get back to you as soon as possible."
   @@base_controller = "::ApplicationController"
+  @@contact_page_path = "contact"
 
   def self.configure(&block)
     yield self
