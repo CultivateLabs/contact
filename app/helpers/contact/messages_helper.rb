c58651e9b1857f5a576296cc493ba66f7d8f64f3
David Van Der Beek <earlynovrock@gmail.com>
2015-03-20 10:42:31 -0500

    add helpers to make form URL's work properly, include helpers in main app

--- app/helpers/contact/messages_helper.rb
+++ app/helpers/contact/messages_helper.rb
@@ -7,5 +7,13 @@ module Contact
         [Contact::Form.find_by(guid: guid), message]
       end
     end
+
+    def form_url(guid)
+      if guid.nil?
+        contact.messages_path
+      else
+        contact.form_messages_path(Contact::Form.find_by(guid: guid))
+      end
+    end
   end
 end
--- lib/contact/engine.rb
+++ lib/contact/engine.rb
@@ -4,6 +4,12 @@ module Contact
   class Engine < ::Rails::Engine
     isolate_namespace Contact
 
+    initializer :include_helpers do |app|
+      ActiveSupport.on_load :action_controller do
+        helper Contact::MessagesHelper
+      end
+    end
+
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
